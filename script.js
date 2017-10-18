4044bd3d38cec36856622d907f55d0a26b0306f6
Estellerky <32845248+Estellerky@users.noreply.github.com>
2017-10-18 17:23:07 +0200

    J2 + J3
    Titre
    Bandeau
    Fond
    Boutons spotify
    Catégories
    Bouton mon compte
    Bouton recherche
    Liens sous pages
    JS
    Bouton animé
    Liste musique Airtable

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $(".moncompte").click(function(){
+      $("header").toggle(1000);
+  });
+
+  $.getJSON("https://api.airtable.com/v0/appSoHqkhqe0oIqB0/musics?api_key=keyLXf2qtgVwYHlLu",
+        function(data) {
+          console.log(data);
+          $.each(data.records, function(key,val) {
+            console.log(val.fields.title);
+
+            $('<li>'+val.fields.title+'</li>').appendTo('#musiques');
+            $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+
+        })
+    }  );
+
+
+        });
