62cfa160f13d21142ab65ee59e00a5d7a94973fd
Estellerky <32845248+Estellerky@users.noreply.github.com>
2017-10-20 14:49:43 +0200

    J5
    Finalisation du site
    Ajout des playlists
    Ajout du Logo
    Arrangement des beugs
    Code propre

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+$( document ).ready(function() {
 
   $.getJSON("https://api.airtable.com/v0/appSoHqkhqe0oIqB0/musics?api_key=keyLXf2qtgVwYHlLu",
         function(data) {
@@ -7,7 +7,7 @@
             console.log(val.fields.title);
 
             $('<li>'+val.fields.title+'</li>').appendTo('#musiques');
-            $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
+            $("<iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe>").appendTo('#musiques');
 
         })
     }  );
